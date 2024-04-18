938a1bc7feda81db3984591f2deac29cda8230be
Sbeier27 <s.beier27@ncf.edu>
2024-04-18 09:09:07 -0400

    Dogs in the works

---
+++ ZenGardenRS/QuestBalloonTemplate-main/ZenGardenRS/Assets/Scripts/PaperFloat.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+public class PaperFloat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
+using UnityEngine;
+
+public class PaperFloat : OVRGrabbable
+{
+    // You need to override the GrabEnd method with the correct signature
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+
+        // Access the Rigidbody component of the grabbed object correctly
+        Rigidbody rb = grabbedRigidbody;
+        if (rb != null)
+        {
+            rb.useGravity = false;
+            rb.AddForce(Vector3.up * 1f, ForceMode.VelocityChange); // Using ForceMode.VelocityChange to ensure consistent behavior
+        }
+    }
+}
