a88796b540d4ca7a22adff71cd3f36aa61044913
Sbeier27 <s.beier27@ncf.edu>
2024-04-25 17:31:11 -0400

    Paper Lantern update

--- ZenGardenRS/QuestBalloonTemplate-main/ZenGardenRS/Assets/Scripts/PaperFloat.cs
+++ ZenGardenRS/QuestBalloonTemplate-main/ZenGardenRS/Assets/Scripts/PaperFloat.cs
@@ -24,25 +24,12 @@ public class PaperFloat : OVRGrabbable
 {
     public Light light;
     public ScoreKeeper scoreKeeper;
-    public Text Zenscore;
-    public Text Score;
-    // You need to override the GrabEnd method with the correct signature
-    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
-    {
-        base.GrabEnd(linearVelocity, angularVelocity);
 
-        /*
- *         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-rb.isKinematic = m_grabbedKinematic;
-rb.velocity = linearVelocity;
-rb.angularVelocity = angularVelocity;
-m_grabbedBy = null;
-m_grabbedCollider = null;
-*/
 
-        // Access the Rigidbody component of the grabbed object correctly
+    // Access the Rigidbody component of the grabbed object correctly
+    void OnCollisionEnter(Collision collision){
         Rigidbody rb = grabbedRigidbody;
-        if (rb != null)
+        if (collision.gameObject.name == "CustomHandRight")
         {
             rb.useGravity = false;
             rb.AddForce(Vector3.up * 1f, ForceMode.VelocityChange); // Using ForceMode.VelocityChange to ensure consistent behavior
