ea153e37c7c5c892df4b672e2957ca5c5110f51d
Sbeier27 <s.beier27@ncf.edu>
2024-04-30 09:44:36 -0400

    Scripting exam improvements
    changed scripts to work for the exam

--- ZenGardenRS/QuestBalloonTemplate-main/ZenGardenRS/Assets/Scripts/DogFollow.cs
+++ ZenGardenRS/QuestBalloonTemplate-main/ZenGardenRS/Assets/Scripts/DogFollow.cs
@@ -8,41 +8,72 @@ public class DogFollow : MonoBehaviour
     public float Dis;
     public Transform target;
     public Animator animator;
+<<<<<<< Updated upstream
+=======
+    public UnityEngine.UI.Text petFed;
+    public UnityEngine.UI.Text message;
+    //public UnityEngine.UI.Text counter;
+    //public int zenLevel;
+    //public UnityEngine.UI.Text Congrats;
+    public Text Zenscore;
+    public Text Score;
+
+    public AudioSource source;
+    public AudioClip clip;
+    //Added during debug
+    public ScoreKeeper scoreKeeper;
+>>>>>>> Stashed changes
 
     // Update is called once per frame
     void Start()
     {
+<<<<<<< Updated upstream
         gameObject.GetComponent<Animator>(); 
+=======
+        animator = gameObject.GetComponent<Animator>();
+>>>>>>> Stashed changes
 
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     void Update()
     {
         Dis = Vector3.Distance(transform.position, Player.transform.position);
 
         if (Dis >= 5)
-        { 
+        {
             transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, 5 * Time.deltaTime);
             gameObject.GetComponent<Animator>().Play("DogWalk");
             if (target != null)
             {
                 transform.LookAt(target);
-                
+
             }
 
 
         }
 
-        
+
 
     }
 
     public void OnCollisionEnter(Collision collision)
     {
+<<<<<<< Updated upstream
+=======
+        if (collision.gameObject.name == "CustomHandRight")
+        {
+            gameObject.GetComponent<Animator>().Play("DogPet");
+            scoreKeeper.IncrementZenScore();
+            //counter.text = (int.Parse(counter.text) + 1).ToString();
+            source.PlayOneShot(clip);
+            message.text = "Having a good day";
+
+            Invoke("ClearText:", 1.5f);
+>>>>>>> Stashed changes
 
         gameObject.GetComponent<Animator>().Play("DogPet");
 
@@ -50,6 +81,11 @@ public class DogFollow : MonoBehaviour
 
 
 
+    }
+
+    public void ClearText() 
+    {
+        message.text = "";
     }
 
 
--- ZenGardenRS/QuestBalloonTemplate-main/ZenGardenRS/Assets/Scripts/PaperFloat.cs
+++ ZenGardenRS/QuestBalloonTemplate-main/ZenGardenRS/Assets/Scripts/PaperFloat.cs
@@ -22,10 +22,15 @@ using UnityEngine;
 public class PaperFloat : OVRGrabbable
 {
     public Light light;
+<<<<<<< Updated upstream
     // You need to override the GrabEnd method with the correct signature
     public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
     {
         base.GrabEnd(linearVelocity, angularVelocity);
+=======
+    public ScoreKeeper scoreKeeper;
+    public UnityEngine.UI.Text message;
+>>>>>>> Stashed changes
 
         // Access the Rigidbody component of the grabbed object correctly
         Rigidbody rb = grabbedRigidbody;
@@ -34,7 +39,18 @@ public class PaperFloat : OVRGrabbable
             rb.useGravity = false;
             rb.AddForce(Vector3.up * 1f, ForceMode.VelocityChange); // Using ForceMode.VelocityChange to ensure consistent behavior
             light.intensity = 3f;
+<<<<<<< Updated upstream
             
+=======
+            scoreKeeper.IncrementZenScore();
+            message.text = "Peace";
+            Invoke("ClearText", 1.5f);
+>>>>>>> Stashed changes
         }
+    }
+
+    public void ClearText()
+    {
+        message.text = "";
     }
 }
